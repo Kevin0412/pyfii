1f9854944838e46946514b00c73eca25c167c14c
Kevin0412 <45508696+Kevin0412@users.noreply.github.com>
2021-11-21 20:52:46 +0800

    Add files via upload

--- drone.py
+++ drone.py
@@ -6,8 +6,8 @@ class Drone:
         self.outputString = '''<xml xmlns="http://www.w3.org/1999/xhtml">
   <variables></variables>
 '''
-        self.X=x
-        self.Y=y
+        self.X=int(x+0.5)
+        self.Y=int(y+0.5)
         self.times=[]
     
     def takeoff(self,time,z):
@@ -15,6 +15,7 @@ class Drone:
         起飞(x坐标,y坐标,起飞高度)
         单位:cm
         """
+        time=int(time+0.5)
         self.times.append(time)
         m=int(time/60) #分
         s = time%60 #秒
@@ -36,7 +37,7 @@ class Drone:
                 <field name="time">1000</field>
                 <next>
                   <block type="Goertek_TakeOff">
-                    <field name="alt">'''+str(z)+'''</field>
+                    <field name="alt">'''+str(int(z+0.5))+'''</field>
 '''
         self.block+=6
         self.inT=True
@@ -83,6 +84,7 @@ class Drone:
         单位:cm
         必须在intime(time)中
         """
+        x,y,z=int(x+0.5),int(y+0.5),int(z+0.5)
         self.x, self.y, self.z = x, y, z
         spaces='  '*(self.space+self.block)
         if self.inT:
@@ -104,6 +106,7 @@ class Drone:
         单位:ms
         必须在intime(time)中
         """
+        time=int(time+0.5)
         spaces='  '*(self.space+self.block)
         if self.inT:
             self.outputString += spaces+'''<next>
@@ -116,6 +119,131 @@ class Drone:
 '''
         self.block+=1
         self.inT=True
+    
+    def ValXY(self,v,a):
+        """
+        水平速度（速度,加速度）
+        单位:cm/s,cm/s^2
+        必须在intime(time)中
+        """
+        v,a=int(v+0.5),int(a+0.5)
+        spaces='  '*(self.space+self.block)
+        if self.inT:
+            self.outputString += spaces+'''<next>
+'''
+            self.block+=1
+            spaces+='  '
+        self.outputString += spaces+'''<block type="Goertek_HorizontalSpeed">
+'''+spaces+'''  <field name="VH">'''+str(v)+'''</field>
+'''+spaces+'''  <field name="AH">'''+str(a)+'''</field>
+'''
+        self.block+=1
+        self.inT=True
+
+    def ValZ(self,v,a):
+        """
+        竖直速度（速度,加速度）
+        单位:cm/s,cm/s^2
+        必须在intime(time)中
+        """
+        v,a=int(v+0.5),int(a+0.5)
+        spaces='  '*(self.space+self.block)
+        if self.inT:
+            self.outputString += spaces+'''<next>
+'''
+            self.block+=1
+            spaces+='  '
+        self.outputString += spaces+'''<block type="Goertek_VerticalSpeed">
+'''+spaces+'''  <field name="VV">'''+str(v)+'''</field>
+'''+spaces+'''  <field name="AV">'''+str(a)+'''</field>
+'''
+        self.block+=1
+        self.inT=True    
+
+    def AccXY(self,a):
+        """
+        水平加速度（加速度）
+        单位:cm/s,cm/s^2
+        必须在intime(time)中
+        """
+        a=int(a+0.5)
+        spaces='  '*(self.space+self.block)
+        if self.inT:
+            self.outputString += spaces+'''<next>
+'''
+            self.block+=1
+            spaces+='  '
+        self.outputString += spaces+'''<block type="Goertek_HorizontalAcceleration">
+'''+spaces+'''  <field name="AH">'''+str(a)+'''</field>
+'''
+        self.block+=1
+        self.inT=True   
+
+    def AccZ(self,a):
+        """
+        竖直加速度（加速度）
+        单位:cm/s^2
+        必须在intime(time)中
+        """
+        a=int(a+0.5)
+        spaces='  '*(self.space+self.block)
+        if self.inT:
+            self.outputString += spaces+'''<next>
+'''
+            self.block+=1
+            spaces+='  '
+        self.outputString += spaces+'''<block type="Goertek_VerticalAcceleration">
+'''+spaces+'''  <field name="AV">'''+str(a)+'''</field>
+'''
+        self.block+=1
+        self.inT=True   
+
+    def ARate(self,w):
+        """
+        角速度（角速度）
+        单位:°/s
+        必须在intime(time)中
+        """
+        w=int(w+0.5)
+        spaces='  '*(self.space+self.block)
+        if self.inT:
+            self.outputString += spaces+'''<next>
+'''
+            self.block+=1
+            spaces+='  '
+        self.outputString += spaces+'''<block type="Goertek_AngularVelocity">
+'''+spaces+'''  <field name="w">'''+str(w)+'''</field>
+'''
+        self.block+=1
+        self.inT=True
+
+    def Yaw2(self,a):
+        """
+        转向（角度）
+        单位:°（左正右负）
+        必须在intime(time)中
+        """
+        spaces='  '*(self.space+self.block)
+        if self.inT:
+            self.outputString += spaces+'''<next>
+'''
+            self.block+=1
+            spaces+='  '
+        if a>0:
+            a=int(a+0.5)
+            self.outputString += spaces+'''<block type="Goertek_TurnTo">
+'''+spaces+'''  <field name="turnDirection">l</field>
+'''+spaces+'''  <field name="angle">'''+str(a)+'''</field>
+'''
+        else:
+            a=int(abs(a)+0.5)
+            self.outputString += spaces+'''<block type="Goertek_TurnTo">
+'''+spaces+'''  <field name="turnDirection">r</field>
+'''+spaces+'''  <field name="angle">'''+str(a)+'''</field>
+'''
+        self.block+=1
+        self.inT=True
+
     def land(self):
         """
         降落
@@ -140,21 +268,28 @@ class Drone:
 '''
 
 if __name__=='__main__':
-    file = open('test.xml',"w")
+    
     d = Drone(100,100)
     d.takeoff(0,100)
     d.intime(3)
+    d.ValXY(200,400)
+    d.ValZ(200,400)
     d.move2(200,200,150)
     d.delay(1000)
     d.move2(250,250,125)
     #d.endtime()
     d.intime(10)
+    d.AccXY(100)
+    d.AccZ(100)
+    d.ARate(120)
+    d.Yaw2(90)
     d.move2(300,300,175)
     #d.endtime()
     d.intime(15)
     d.land()
     #d.endtime()
     d.end()
+    file = open('test.xml',"w")
     file.write(d.outputString)
     file.close()
     print(d.times)
---
+++ read_img.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+def main(img,z=(80,250)):
+    img=cv2.resize(img,(560,560))
+    def get_point(event,x,y,flags,param):
+        if event==cv2.EVENT_LBUTTONDOWN:
+            print('d.move2('+str(560-y)+','+str(x)+','+str(int(z[0]*x/560+z[1]*(560-x)/560+0.5))+')')
+
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image',get_point)
+    while(True):
+        cv2.imshow('image',img)
+        if cv2.waitKey(20)&0xFF==27:
+            break
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main(cv2.imread('test2.jpg'))
