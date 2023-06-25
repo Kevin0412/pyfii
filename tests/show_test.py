bccd0051ccd6637f7747084c61b886d8bdebb96a
Kevin0412 <45508696+Kevin0412@users.noreply.github.com>
2023-06-26 00:23:03 +0800

    文档修订

--- tests/show_test.py
+++ tests/show_test.py
@@ -5,13 +5,7 @@ sys.path.append(path)
 
 import pyfii as pf
 
-<<<<<<< HEAD
 name='output/1'
 data,t0,music,feild=pf.read_fii(name,getfeild=True,fps=60)
 pf.show(data,t0,music,feild=feild,save=name,FPS=60,max_fps=60)
 pf.show(data,t0,music,feild=feild,save=name+'_3D',ThreeD=True,imshow=[90,3],d=(600,500),FPS=60,max_fps=60)
-=======
-name='output/...'
-data,t0,music=pf.read_fii(name)
-pf.show(data,t0,music)
->>>>>>> 28b7c2174f689a22da54a0bdb491fa9b77a28eca
